75981c8869483b53612f9b9cfe3aa5175035cb60
Acendor <al-ex@nurfuerspam.de>
2015-11-12 22:28:08 +0100

    added nginx removed apache from loadbalancer

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,14 +1,33 @@
 node 'loadbalancer' {
+  class { 'nginx':}
+  nginx::resource::upstream { 'puppet_web':
+    members => [
+      'localhost:3000',
+      'localhost:3001',
+      'localhost:3002',
+    ],
+  }
+
+  nginx::resource::vhost { 'www.puppet.local':
+    proxy => 'http://puppet_web',
+  }
+  package { 'apache2':
+    ensure => purged,
+  }
+}
+
+node 'node' {
   class { 'apache': }
   apache::vhost { 'example.com':
     port => '80',
     docroot => '/var/www/html'
   }
 }
+
 node 'mysql' {
   class { '::mysql::server':
-	root password => 'EinMannDerSichKolumbusNannt',
-	remove default accounts => true,
+	root_password => 'EinMannDerSichKolumbusNannt',
+	remove_default_accounts => true,
 	override_options => $override_options
   }
 }
