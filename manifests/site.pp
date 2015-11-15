8c54a0f9c6023251bff1d2fb9d8b184d9b6e24d6
root <root@puppetmaster.puppet>
2015-11-15 13:39:30 +0100

    webnode 2 added (CentOS)

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,7 +2,8 @@ node 'loadbalancer' {
   class { 'nginx':}
   nginx::resource::upstream { 'puppet_web':
     members => [
-      'webnode.puppet.local',
+      'webnode2.puppet',
+	'webnode.puppet', 
     ],
   }
 
@@ -18,13 +19,22 @@ node 'webnode' {
     docroot => '/var/www/html'
   }
 }
+
+node 'webnode2' {
+  class { 'apache': }
+  apache::vhost { 'webnode.puppet.local':
+    port => '80',
+    docroot => '/var/www/html'
+  }
+}
+
 node 'mysql' {
 class { '::mysql::server':
   create_root_user 	  => true,
   root_password           => 'EinMannDerSichKolumbusNannt',
   remove_default_accounts => true,
   service_enabled	  => true,
-  override_options        => $override_options 	
+  override_options        => $override_options
   }
 }
 
